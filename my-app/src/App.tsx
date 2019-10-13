65ece05de624e79599696de92966c7cbe2a9439e
Rio Ohashi <jkljkl5665@gmail.com>
2019-10-12 23:55:19 -0700

    add: added service links with logo.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import flogo from './f_logo.png';
+import lilogo from './LI_logo.png';
+import ghlogo from './GitHub_logo.png';
+import { MailOutline, AspectRatio, CloudOutlined, Build, DesktopWindowsRounded } from '@material-ui/icons/';
+import profileImg from './IMG_0980.jpg';
 import './App.scss';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>
+          <a href="https://m.facebook.com/monotori333"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src={flogo} className="logo-link" />
+          </a>
+          <a href="https://www.linkedin.com/in/rio-ohashi/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src={lilogo} className="logo-link" />
+          </a>
+          <a href="https://github.com/rio-ohashi"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src={ghlogo} className="logo-link" />
+          </a>
+          <a href="https://github.com/rio-ohashi"
+            target="_blank"
+            rel="noopener noreferrer">
+            <MailOutline fontSize="large" color="action" className="logo-link" />
+          </a></span>
       </header>
     </div>
   );
